1e28090b79883aad4493b20d0390cb29244fb345
Sravan kumar Velanati <sravanworkinfo@gmail.com>
2023-11-11 12:25:16 -0600

    activity updates

---
+++ app/src/main/java/com/sravan/covidapplication/Activities/GitTestActivity.kt
@@ -0,0 +1,12 @@
+package com.sravan.covidapplication.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sravan.covidapplication.R
+
+class GitTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_test)
+    }
+}
