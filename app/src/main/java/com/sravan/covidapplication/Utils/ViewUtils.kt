28980b75391b5dff43dd2400fcc11f164af0b5ff
Sravan kumar Velanati <sravanworkinfo@gmail.com>
2024-01-03 13:42:42 -0600

    "fourth commit"

--- app/src/main/java/com/sravan/covidapplication/Utils/ViewUtils.kt
+++ app/src/main/java/com/sravan/covidapplication/Utils/ViewUtils.kt
@@ -20,4 +20,5 @@ fun showSnackLongBar(view: View, message: String) {
     Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
     println("This is first commit")
     println()
+    println("fourth")
 }
