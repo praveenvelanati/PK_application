58769dfd89748f71477558f27eaf7ced2fc4e1eb
Sravan kumar Velanati <sravanworkinfo@gmail.com>
2024-01-03 14:20:00 -0600

    commit three

--- app/src/main/java/com/sravan/covidapplication/Utils/ViewUtils.kt
+++ app/src/main/java/com/sravan/covidapplication/Utils/ViewUtils.kt
@@ -20,4 +20,5 @@ fun showSnackLongBar(view: View, message: String) {
     Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
     println("first commit")
     println("second commit")
+    webkjwbfkrekve
 }
